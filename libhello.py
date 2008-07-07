39265b182c9383db275aa70ae60ef75201614002
Asheesh Laroia <asheesh@asheesh.org>
2008-07-07 15:55:56 -0700

    Based on Felix Riemann post from 10 Jun 2008 to eog-list

--- libhello.py
+++ libhello.py
@@ -1,3 +1,12 @@
-if __name__ == '__main__':
-    print 'The answer landed on my rooftop, whoa'
+import eog
+
+class HelloWorldPlugin(eog.Plugin):
+        def __init__(self):
+                eog.Plugin.__init__(self)
+
+        def activate(self, window):
+                print 'The answer landed on my rooftop, whoa'
+
+        def deactivate(self, window):
+                print 'The answer fell off my rooftop, woot'
 
