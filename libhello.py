c832c87c62eefc074cf8d72311f0d47417b8e945
Asheesh Laroia <asheesh@asheesh.org>
2008-07-07 17:10:55 -0700

    Show a menu bar item

--- libhello.py
+++ libhello.py
@@ -1,12 +1,32 @@
 import eog
+import gtk
 
 class HelloWorldPlugin(eog.Plugin):
+        _ui_str = '''
+        <ui>
+            <menubar name="MainMenu">
+                <menu name="ToolsMenu" action="Tools">
+                    <separator/>
+                    <menuitem name="PythonConsole" action="PythonConsole"/>
+                    <separator/>
+                </menu>
+            </menubar>
+        </ui>'''
         def __init__(self):
                 eog.Plugin.__init__(self)
 
         def activate(self, window):
+                ui_manager = window.get_ui_manager()
+                group = gtk.ActionGroup('PythonConsole')
+                group.add_actions([('PythonConsole', None, 'P_ython Console',
+                    None, None, self.console_cb)], window)
+                ui_manager.insert_action_group(group, 0)
+                ui_id = ui_manager.add_ui_from_string(self._ui_str)
+
                 print 'The answer landed on my rooftop, whoa'
 
         def deactivate(self, window):
                 print 'The answer fell off my rooftop, woot'
 
+        def console_cb(self, action, window):
+            print "Calling me to work it out."
